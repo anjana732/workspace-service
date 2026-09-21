a716fd7aae6e735c7c5fc0c16cf7aedc3a482943
anjana732 <anjanasingh1257@gmail.com>
2026-09-21 19:57:10 +0530

    initialize workspace service

---
+++ src/main/java/com/workspace/service/WorkspaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.workspace.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceServiceApplication.class, args);
+	}
+
+}
